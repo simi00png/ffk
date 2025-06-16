cc2fa071c4a687e0dc08c0612ad5ee265ca2a80a
simi00png <simimd7773@gmail.com>
2025-06-16 16:37:52 +0100

    Add files via upload

---
+++ plugins/alive.js
@@ -0,0 +1,46 @@
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "runtime", "uptime"],
+    desc: "Check uptime and system status",
+    category: "main",
+    react: "⌚",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Generate system status message
+        const status = `
+╭──〔KAVIYA MD〕───·๏
+┃🛸┃• *⏳ Uptime*:  ${runtime(process.uptime())} 
+┃🛸┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}GB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}TB
+┃🛸┃• *⚙️ HostName*: ${os.hostname()}
+┃🛸┃• *👨‍💻 Creator*: 𝙱.𝙼.𝙱-𝚇𝙼𝙳
+┃🛸┃• *🧬 Version*: 1.0.0
+╰──────────────┈⊷
+> © KAVIYA MD`;
+
+        // Send the status message with an image
+        await conn.sendMessage(from, { 
+            image: { url: `https://i.ibb.co/201XgZ5f/SulaMd.jpg` },  
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363382023564830@newsletter',
+                    newsletterName: 'KAVIYA MD🔥',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Error in alive command:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
