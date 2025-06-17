ca265bf695887c1637e1e30ba9464478ac5c49ee
simi00png <simimd7773@gmail.com>
2025-06-17 12:09:00 +0530

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -7,7 +7,7 @@ const createLogo = async (bot, from, message, styleUrl, name) => {
     let apiResponse = await fetchJson(`https://api-pink-venom.vercel.app/api/logo?url=${styleUrl}&name=${name}`);
     await bot.sendMessage(from, {
       'image': { 'url': apiResponse.result.download_url },
-      'caption': "> *©🔥 𝐁.𝐌.𝐁-𝐗𝐌𝐃 🔥*"
+      'caption': "> *©🔥 SIMI MD 🔥*"
     }, { quoted: message });
   } catch (error) {
     console.error(error);
